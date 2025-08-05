e4ad1339ad0cf86b8d026db7c588e79b405b7c4a
Eugene <156468626+CTpaHHuK88@users.noreply.github.com>
2025-08-05 16:07:31 +0300

    Delete conff.py

--- conff.py
+++
@@ -1,6 +0,0 @@
-class DataTelebot():
-    def __init__(self):
-        self.TOKEN = '7798207878:AAGgOBxvmHgeuufHIYEJWaDbVurehWHQRR8'
-        self.USERS_ID = [
-            996707444,
-            5840916530]
