ce92ae2a9e545aa0a64279db73f470780aa61be6
Rudolf <e8338880@yandex.ru>
2025-08-05 16:05:55 +0300

    upd_05.08.2025

--- conff.py
+++ conff.py
@@ -1,6 +1,6 @@
 class DataTelebot():
     def __init__(self):
-        self.TOKEN = ''
-        #Белый список пользователей, которе имеют допуск. Можно указать через запятую.
+        self.TOKEN = '7798207878:AAGgOBxvmHgeuufHIYEJWaDbVurehWHQRR8'
         self.USERS_ID = [
-]
+            996707444,
+            5840916530]
